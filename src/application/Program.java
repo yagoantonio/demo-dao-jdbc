1a1117387513f531cefcfd6a23e414152d201594
Yago Antonio <yago_antonio@live.com>
2020-02-10 15:36:34 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -38,10 +41,18 @@ public class Program {
 		
 		System.out.println("\n==== TEST 5: seller update ====");
 		seller = sellerDao.findById(1);
-		seller.setName("Martha Waine");
+		seller.setName("Bob Brown");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 		
+		System.out.println("\n==== TEST 5: seller deleteById ====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -69,7 +69,6 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public void update(Seller obj) {
 		PreparedStatement st = null;
-		ResultSet rs = null;
 		
 		try {		
 			st = conn.prepareStatement(
@@ -91,14 +90,30 @@ public class SellerDaoJDBC implements SellerDao {
 			throw new DbException(e.getMessage());  
 		}
 		finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
+		PreparedStatement st = null;
 		
+		try {		
+			st = conn.prepareStatement(
+					"DELETE FROM seller WHERE Id = ?"
+			);
+			
+		st.setInt(1, id);
+		
+		st.executeUpdate();
+		
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());  
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
